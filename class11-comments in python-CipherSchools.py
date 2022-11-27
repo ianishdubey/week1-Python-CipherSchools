27f0a4d028c75b887fb5c85227777afc8a0b50d9
Anish Dubey <anishdubey300@gmail.com>
2022-11-27 13:31:13 +0530

    First Commit

---
+++ class11-comments in python-CipherSchools.py
@@ -0,0 +1,8 @@
+
+# This line is commented by the use of '#' 
+# Using '#' by putting it in front of line
+
+# Print function
+print("Badshah")
+
+# Shortcut to comment any line is ('ctrl' + 'forward slash')
---
+++ class9-print() function-CipherSchools.py
@@ -0,0 +1,10 @@
+#Different ways of using double quotes and single quotes
+print("hello world")
+
+#Single quotes can be used in double quotes
+print("hello 'world'")
+
+#Double quotes can be used in single quotes
+print('hello "world"')
+
+#we cannot use single quotes in between single quotes and double quotes in between double quotes
