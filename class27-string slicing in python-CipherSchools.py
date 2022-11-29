bdedaacd850bc8f50ae324a1315a919fa32e67a4
Anish Dubey <anishdubey300@gmail.com>
2022-11-29 20:09:55 +0530

    class27-string slicing in python-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class27-string slicing in python-CipherSchools.py
@@ -0,0 +1,10 @@
+# Slicing/ selecting sub sequences
+lang="Python"
+
+#print(lang[4])<-- this can print only one word
+
+#synatx-[start argument : stop argument-1]<-- this will print words from starting argument to stoping argument
+print(lang[3:6])#this will print words starting from indexing number 3 to 6
+print(lang[-3:6])
+print(lang[:])#this will print whole word
+print(lang[:3])#this will print word from start and upto 3
