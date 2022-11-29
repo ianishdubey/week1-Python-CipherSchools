16182b83e2a4bda03b0b14934a0947b464548455
Anish Dubey <anishdubey300@gmail.com>
2022-11-29 20:30:19 +0530

    class29-Chapter 2 Exercise 2-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class29-chapter 2 Exercise 2-CipherSchools.py
@@ -0,0 +1,10 @@
+#Chapter 2-->Exercise 2
+
+
+#ask user name and print back user name in reverse order
+
+#try to make your program in two lines only
+
+name=input("enter your name: ")# input is taken from user
+print(f"Reverse of your name is {name[::-1]}")#reverse of name ahs been given using step argument method
+
