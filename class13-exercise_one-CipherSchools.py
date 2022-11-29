788243f5494df337c6396eb6efa44fd1e681a525
Anish Dubey <anishdubey300@gmail.com>
2022-11-29 15:33:44 +0530

    class13-exercise_one-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class13-exercise_one-CipherSchools.py
@@ -0,0 +1,13 @@
+# print these lines
+
+#this is double backslash \\
+print("this is double backslash \\\\")
+
+#this is /\/\/\/\/\ mountain
+print("this is /\\/\\/\\/\\/\\ mountain")
+
+#he is    awesome (use escape sequence instead of manual spaces)
+print("this is \t awesome")
+
+# \" \n \t \' (print this as an output)
+print(" \\\" \\n \\t \\'")
