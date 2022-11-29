878e6d23fa87149fa561be9d640c93b029b1ac24
Anish Dubey <anishdubey300@gmail.com>
2022-11-29 18:37:05 +0530

    class21-more than one variables in python-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class21-more about variables in python-CipherSchools.py
@@ -0,0 +1,7 @@
+# in one line we can declare more than value to variables
+name,age="Demon","24" #24 is in double commas so that it should be of string type
+print("hello "+name+" your age is " + age)
+
+# in one line we can declare one same value to more than one variable
+x=y=z=1
+print(x+y+z)
