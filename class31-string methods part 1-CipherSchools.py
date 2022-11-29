b12269fe8d66a6414709dc1c11192b7bc9e2bd3f
Anish Dubey <anishdubey300@gmail.com>
2022-11-29 21:21:22 +0530

    class31-string methods part 1-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class31-string methods part 1-CipherSchools.py
@@ -0,0 +1,19 @@
+#string methods part 1
+
+name="Anish Dubey"
+
+#1. len function <-- this will tell about the length of the string
+length=len(name)
+print(length)
+
+#2. lower() function <-- this will gives the input string in small letters
+print(name.lower())
+
+#3. upper() function <-- this will gives the input string in capital letters
+print(name.upper())
+
+#4. title method <-- this method will gives string as output with its first letter as capital
+print(name.title())
+
+#5. count method <-- by this method we can find that how much times any letter is repeated in a word
+print(name.count("h"))
