28d977ec20ed1e9705411356577a6501e4f09c5e
Anish Dubey <anishdubey300@gmail.com>
2022-11-29 19:15:55 +0530

    class23-string formatting-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class23-string formatting-CipherSchools.py
@@ -0,0 +1,15 @@
+name= "Goku"
+age= 15
+print("hello "+ name + "your age is " + str(age)) # Too much ugly syntax
+#Three types of string formatting
+#Python 2
+#Python 3
+#Python 3.6
+
+print("hello {} your age is {}".format(name,age)) # Python 3 string formatting style
+
+print(f"hello {name} your age is {age+2} ") # clean syntax , Python 3.6 formatting style
+
+# we can do some calculations also while using Python 3.6 and in Python 3 formatting style
+
+print(f"hello{name} your age is {age+2}")
