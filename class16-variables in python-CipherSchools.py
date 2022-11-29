75b58cf76d01a7801efb70390a3254be80784cf1
Anish Dubey <anishdubey300@gmail.com>
2022-11-29 16:38:56 +0530

    class16-variables in  python-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class16-variables in python-CipherSchools.py
@@ -0,0 +1,43 @@
+# Variables are something that act as container that store some value in it
+#Values stored in variables can be of string type or number type
+
+var1=34
+print(var1)
+
+go2="143"
+print(go2)
+
+#Variable value can be changed 
+#If we give some another value with the same variable name that was already used then that variable will print new value instead of printing previous value
+zee="943"
+print(zee)
+zee="69" 
+print(zee)
+# As value of zee is changed so now new value of zee will get print that is "69 not "943"
+
+
+#any word stored in variable should be in double commas
+soc="CR"
+print(soc)
+
+#some rules of variables
+#1. variable should not start with the number but number can be used in between name
+# 1num="few" <-- it will give error
+nu1m="fewer"
+print(nu1m)
+
+#2. variable name can start with underscore
+_info = "variable name can start with underscore"
+print(_info)
+
+#3. variable name cannot start with special symbol and also it cannot be used in between of variable name
+# @love = "I like this feeling"<-- it will give error
+#ha@te = "This is also good feeling"<--it also give error
+
+#Types of variable writing
+#1. Snake_case_writing <-- in this writing underscore is used between different words
+Devil_eve_adam = "bash"
+print(Devil_eve_adam)
+#2. camel Case Writing <-- in this writing first letter of variable's name is lowercase and variable's name other word's letter is upper case
+birthdayBash = "lts"
+print(birthdayBash)
