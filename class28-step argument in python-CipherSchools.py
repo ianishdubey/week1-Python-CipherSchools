e74835ec656ef4ad348fe4bb19721e47465d9744
Anish Dubey <anishdubey300@gmail.com>
2022-11-29 20:20:23 +0530

    class28-step argument in python-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class28-step argument in python-CipherSchools.py
@@ -0,0 +1,9 @@
+# Step arguments in python
+#syntax - [start argument : stop argument-1 : step]
+print("linguistic"[0:5:2])
+
+#by step argument word can be print in reverse
+print("logistic"[-1::-1])
+
+#another way of reversing string
+print("logistic"[::-1])
