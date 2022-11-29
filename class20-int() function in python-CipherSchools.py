3be860437b92510f806f9a97b5d9b440123676b5
Anish Dubey <anishdubey300@gmail.com>
2022-11-29 18:17:45 +0530

    class20-int() function in python-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class20-int() function in python-CipherSchools.py
@@ -0,0 +1,28 @@
+# adding two user input values that are in strings
+number_one = input("enter first number: ") # 4
+number_two = input ("enter second number: ") # 4
+total = number_one + number_two
+print("total is " + total)
+#total= "4"+"4"=44
+
+# adding two user input values that are in int type
+number_one = int(input("enter first number: ")) # 4
+number_two = int(input("enter second number: ")) # 4
+total = number_one + number_two
+print("total is " + str(total))
+
+# str
+# 4 -->"4"
+
+#int
+# "4" --> 4
+
+# float
+#"4" -->4.0
+
+number1 = str(4)
+number2 = float("44")
+number3 = int("33")
+
+# int + float = float
+print(number2 + number3) #float
