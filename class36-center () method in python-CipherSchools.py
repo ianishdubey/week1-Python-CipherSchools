e6d100bf81f06988bfdb6dba967ca05f84a58fa3
Anish Dubey <anishdubey300@gmail.com>
2022-11-29 23:41:46 +0530

    class36-center () method in python-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class36-center () method in python-CipherSchools.py
@@ -0,0 +1,9 @@
+# center method 
+
+name="Anish"
+# output should be **Anish**
+print(name.center(15,"*"))
+
+#taking input from user and putting stars after before the user's input
+name=input("enter your name: ")
+print(name.center(len(name)+10,"*"))
