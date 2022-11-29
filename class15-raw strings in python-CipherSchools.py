f92a8d7d5e95300eca23c094a053102e7a1aa7d3
Anish Dubey <anishdubey300@gmail.com>
2022-11-29 15:51:58 +0530

    class15-raw strings in python-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class15-raw strings in python-CipherSchools.py
@@ -0,0 +1,10 @@
+# we can use "r" to print "\n" same in a line which means word written after \n will not get print in next line
+
+#Some Examples
+print(r"he \n she")
+
+print(r"love \n war")
+
+print(r"adam \n eve")
+
+print(r"demons \n love")
