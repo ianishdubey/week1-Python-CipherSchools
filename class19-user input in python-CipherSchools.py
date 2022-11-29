446508e81b32b2ea9c411a64d6e43a1df12b6349
Anish Dubey <anishdubey300@gmail.com>
2022-11-29 17:51:07 +0530

    class19-user input in python-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class19-user input in python-CipherSchools.py
@@ -0,0 +1,10 @@
+#user input
+#input function
+name=input("type your name: ")
+print("Hello "+name)
+
+#string
+age=input("What is your age?\n ") #input has been taken in the form of string " "
+
+#Proof 
+print("Your age is "+ age) #string concatenates with string only so this shows "age" input has been taken in the form of string instead of taken it in the form of number
