9392e56f85c25cbaa7f8febb379d816e69138e38
Anish Dubey <anishdubey300@gmail.com>
2022-11-30 14:43:40 +0530

    class51-while loop in python-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class51-while loop in python-CipherSchools.py
@@ -0,0 +1,13 @@
+# loops
+# while loop,for loop
+# print("hello world") #10 times
+#using while loop
+i=1  #i=2
+while i<=10:
+    print(f"hello world{i}")
+    i = i + 1    
+
+i=1
+while i<=10:
+    print("hello world")
+    i = i + 1        
