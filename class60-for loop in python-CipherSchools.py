cd2097ae8e73ba86293d852434614d35572d207d
Anish Dubey <anishdubey300@gmail.com>
2022-11-30 18:23:29 +0530

    class60-for loop in python-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class60-for loop in python-CipherSchools.py
@@ -0,0 +1,9 @@
+# for loop with range function
+
+for i in range(10): #0 to 9
+    print("hello world")
+
+
+for i in range(16): # 0 to 15
+    print(f"hello world :{i}")
+    print("this is line \n")
