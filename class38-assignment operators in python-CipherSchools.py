0820b118bea53c4921080e4754089fa8d628765e
Anish Dubey <anishdubey300@gmail.com>
2022-11-30 09:17:14 +0530

    class38-assignment operators in python-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class38-assignment operators in python-CipherSchools.py
@@ -0,0 +1,14 @@
+name="Ani"
+name = name + "sh" # now we might think how we can change name variable if strings are immutable?
+                    #so remember in python we can change value of variable anytime but other methods cannot change our string or we cannot change particular character from string(immutability)
+
+# another way of adding it
+name+="sh"
+print(name)
+
+age=18
+age+=1
+print(age)
+
+age*=100
+print(age)
