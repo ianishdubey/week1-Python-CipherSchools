a38a1e5f3a1b4ea82acc23cd044ffc6bf8728fc1
Anish Dubey <anishdubey300@gmail.com>
2022-11-30 09:57:43 +0530

    class40-if statement in python-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class40-if statement in python-CipherSchools.py
@@ -0,0 +1,11 @@
+# syntax    
+# if condition:
+
+# code #if condition is true then this code will execute
+# code
+
+age=input("enter your age: ")
+age=int(age)
+if age>=14: # here ":" should be placed otherwise we can get error as it is the format of if
+    print("line a")
+    print("you are above 14") #spaces should be concerned otherwise error will come in output
