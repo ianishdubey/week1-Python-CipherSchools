dc9a4f0391be6426c757028a8857e2a679074957
Anish Dubey <anishdubey300@gmail.com>
2022-11-30 12:23:19 +0530

    class49-in keywords in python-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class49-in keywords in python-CipherSchools.py
@@ -0,0 +1,12 @@
+name = "Logistics"
+#  in keyword
+# if with in 
+if 'i' in "Logistics":
+    print("i is present in name")
+else:
+    print("not present")
+
+if 'A' in "Logistics":
+    print("A is present in name")
+else:
+    print("not present")
