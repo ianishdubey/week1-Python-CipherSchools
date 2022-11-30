95ab3c07ac8c681e81e0df269feee194a83e5bc1
Anish Dubey <anishdubey300@gmail.com>
2022-11-30 18:22:06 +0530

    class59-infinite loop in python-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class59-infinite loop in python-CipherSchools.py
@@ -0,0 +1,18 @@
+# infinite loop
+
+i=0
+while i<=10:
+    print("hello world")
+    i += 1 # this will print finite hello world
+
+
+
+while True:
+    print("Hello World") #this will print infinite hello world
+
+
+
+# boolean types:-
+# True
+# False
+# during loop evaluation, loop will either be true or false
