251e9e4b84f4f578f4b599ce70cdc5a22e46008d
Anish Dubey <anishdubey300@gmail.com>
2022-11-30 10:18:34 +0530

    class42-if_else statement in python-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class42-if_else statement in python-CipherSchools.py
@@ -0,0 +1,9 @@
+# if-else condition
+
+age=input("enter your age: ")
+age=int(age)
+if age >=14:
+    print("line a")
+    print("you are above 14")
+else: #else condition comes after if condition , it works when we want to give another condition other than if
+    print("sorry,you can't play") 
