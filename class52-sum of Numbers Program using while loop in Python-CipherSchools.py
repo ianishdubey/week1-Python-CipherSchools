5487d4df0a2c1ec3caac8ea498395442324496f3
Anish Dubey <anishdubey300@gmail.com>
2022-11-30 14:44:38 +0530

    class52-sum of Numbers Program using while loop in Python-CipherSchools
    Signed-off-by: Anish Dubey <anishdubey300@gmail.com>

---
+++ class52-sum of Numbers Program using while loop in Python-CipherSchools.py
@@ -0,0 +1,9 @@
+# sum of numbers from 1 to 10 using while loop
+# by this code we can get sum of n numbers
+total=0
+i=1
+while i<=10:
+    total+=i #total = total+i
+    i+=1
+print(total)
+# total = 1+2+3+4+5+6+7+8+9+10
